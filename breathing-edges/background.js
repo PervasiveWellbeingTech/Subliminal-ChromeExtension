81d07ba00beb0b1cf9bde1d97cf086043afa6247
Marco Mora-Mendoza <marcom3@stanford.edu>
2019-08-19 22:30:35 -0700

    Update background.js

--- breathing-edges/background.js
+++ breathing-edges/background.js
@@ -2,18 +2,20 @@ var user;
 
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
   // Wait for message from web
-  console.log(request.messageFromWeb);
+  console.log(request.messageFromBTWebHost);
+  let hr = request.messageFromBTWebHost[0];
+  let br = request.messageFromBTWebHost[1];
   // Update enabled var
   chrome.storage.sync.get(['enabled', 'adaptive'], function(result) {
       if(result.adaptive == true){
-        if(result.enabled == false && request.messageFromWeb > 75) {
+        if(result.enabled == false && hr > 75) {
           chrome.storage.sync.set({
               enabled: true
           });
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
               chrome.tabs.sendMessage(tabs[0].id, {todo: "update"});
           });
-        } else if (result.enabled == true && request.messageFromWeb <= 75){
+        } else if (result.enabled == true && hr <= 75){
           chrome.storage.sync.set({
               enabled: false
           });
